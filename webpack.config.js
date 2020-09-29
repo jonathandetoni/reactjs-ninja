274b7f218da04cc610c97bd83395ba12fba342e7
Jonathan de Toni <jonathan@tonidev.com.br>
2020-09-29 20:23:58 -0300

    Inclusao da instrucao mode no webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: path.join(__dirname, 'src', 'index'),
     output: {
         path: path.join(__dirname, 'dist'),
