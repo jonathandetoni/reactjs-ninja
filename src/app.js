bcf8af71b2072a68ce88c883ae86cf8be1e9e405
Jonathan de Toni <jonathan@tonidev.com.br>
2020-10-01 17:39:08 -0300

    utilizando o defaultProps

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ class App extends Component {
   render () {
     return (
       <div className='title'>
-        <Title />
+        <Title name='Jonathan' sobrenome={{ first: 'de', last: 'Toni' }} />
       </div>
     )
   }
--- src/title.js
+++ src/title.js
@@ -5,13 +5,17 @@ import React, { Component } from 'react'
 class Title extends Component {
   render () {
     return (
-      <h1>Olá {this.props.name} </h1>
+      <h1>Olá {this.props.name} {this.props.sobrenome.first} {this.props.sobrenome.last}</h1>
     )
   }
 }
 
 Title.defaultProps = {
-  name: 'Desconhecido'
+  name: 'Desconhecido',
+  sobrenome: {
+    first: 'Sem sobrenome',
+    last: ''
+  }
 }
 
 export default Title
