b706cfec5363c9be7769da96d7501a27df0cd526
Jonathan de Toni <jonathan@tonidev.com.br>
2020-10-01 15:17:04 -0300

    remocao da virgula

--- webpack.config.js
+++ webpack.config.js
@@ -4,47 +4,47 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
-    mode: 'development',
-    devtool: 'source-map',
+  mode: 'development',
+  devtool: 'source-map',
 
-    resolve: {
-        alias: {
-          'react-dom': '@hot-loader/react-dom'
-        }
-    },
-
-    entry: [
-        'react-hot-loader/patch',
-        'webpack-dev-server/client?http://localhost:3000',
-        'webpack/hot/only-dev-server',
-        path.join(__dirname, 'src', 'index')
-    ],
-
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
-
-    output: {
-        path: path.join(__dirname, 'dist'),
-        filename: 'bundle.js',
-        publicPath: '/static/',
-    },
-
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                include: /src/,
-                loader: 'standard-loader',
-                enforce: 'pre'
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                include: /src/,
-                use: ['babel-loader']
-            }
-        ]
+  resolve: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
     }
+  },
+
+  entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, 'src', 'index')
+  ],
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ],
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: /src/,
+        loader: 'standard-loader',
+        enforce: 'pre'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        include: /src/,
+        use: ['babel-loader']
+      }
+    ]
+  }
 }
