e6786788b7eb0fa99b7f4aaafdd6d0a63f529e3a
Jonathan de Toni <jonathan@tonidev.com.br>
2020-10-05 15:03:35 -0300

    capturando eventos

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react'
 
-import Title from './title'
 import Square from './square'
 
 class App extends Component {
   render () {
     return (
-      <div className='title'>
-        <Title name='Jonathan' sobrenome={{ first: 'de', last: 'Toni' }} />
-        {['red', 'blue', 'yellow'].map((square) => (
-          <Square color={square} key={square} />
-        ))}
+      <div className='title' onClick={function (e) {
+        alert('clicou')
+      }}>
+        <Square />
       </div>
     )
   }
